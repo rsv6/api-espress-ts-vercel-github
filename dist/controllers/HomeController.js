c6ab52f03d9c70033c7c7eff9e3501fc1ab1bdc0
Rafael Santos <rafael.santos@acal.local>
2024-03-17 17:57:57 -0300

    feat: add project gitub to vercel - add route users -2

--- dist/controllers/HomeController.js
+++ dist/controllers/HomeController.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.HomeController = void 0;
 const HomeController = (req, res) => {
-    return res.json({ msg: 'Hello World!' });
+    return res.json({ msg: 'Hello World Vecel, here was modify return!' });
 };
 exports.HomeController = HomeController;
--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -2,5 +2,5 @@ import { Request, Response } from "express";
 
 export const HomeController = (req: Request, res: Response) => {
 
-    return res.json({ msg: 'Hello World!' });
+    return res.json({ msg: 'Hello World Vecel, here was modify return!' });
 }
