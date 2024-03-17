b1cbc2901fefd48c2a11566bae7db5f235619a87
Rafael Santos <rafael.santos@acal.local>
2024-03-17 17:28:48 -0300

    feat: add project gitub to vercel - 3

---
+++ dist/controllers/HomeController.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HomeController = void 0;
+const HomeController = (req, res) => {
+    return res.json({ msg: 'Hello World!' });
+};
+exports.HomeController = HomeController;
